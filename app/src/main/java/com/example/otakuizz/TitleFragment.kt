2fc27ef9fd2938eb5809c595a02d3be15ff701a4
Gonzalo <gonzalomerida@hotmail.com>
2023-12-21 11:39:45 +0100

    incluido tema de menu items

--- app/src/main/java/com/example/otakuizz/TitleFragment.kt
+++ app/src/main/java/com/example/otakuizz/TitleFragment.kt
@@ -3,15 +3,21 @@ package com.example.otakuizz
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.MenuHost
+import androidx.core.view.MenuProvider
+import androidx.lifecycle.Lifecycle
 import androidx.navigation.fragment.findNavController
 import com.example.otakuizz.databinding.FragmentTitleBinding
 
 
 class TitleFragment : Fragment() {
 
-    private var _binding : FragmentTitleBinding? = null
+    private var _binding: FragmentTitleBinding? = null
     val binding
         get() = _binding!!
 
@@ -29,12 +35,41 @@ class TitleFragment : Fragment() {
         // Inflate the layout for this fragment
         _binding = FragmentTitleBinding.inflate(inflater, container, false)
 
-        binding.btnPlay.setOnClickListener{
+        binding.btnPlay.setOnClickListener {
             findNavController().navigate(R.id.action_titleFragment_to_gameFragment)
         }
 
 
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+        //para crear menus siempre es así, cambiando las variables
+        val menuHost: MenuHost = requireActivity()
+        menuHost.addMenuProvider(object : MenuProvider {
+            override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+                menuInflater.inflate(R.menu.options_menu, menu)
+            }
+
+            override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+                return when (menuItem.itemId) {
+                    R.id.about_menu -> {
+                        findNavController().navigate(R.id.action_titleFragment_to_aboutFragment)
+                        true
+                    }
+
+                    R.id.rules_menu -> {
+                        findNavController().navigate(R.id.action_titleFragment_to_rulesFragment)
+                        true
+                    }
+
+                    else -> false
+                }
+            }
+        }, viewLifecycleOwner, Lifecycle.State.RESUMED)
     }
 
 }
