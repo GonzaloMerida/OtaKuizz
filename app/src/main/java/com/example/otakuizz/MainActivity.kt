38843a6218d48a176071260d941ee79d78b81ff1
Gonzalo <gonzalomerida@hotmail.com>
2023-12-21 11:34:53 +0100

    incluido tema de menu items

--- app/src/main/java/com/example/otakuizz/MainActivity.kt
+++ app/src/main/java/com/example/otakuizz/MainActivity.kt
@@ -2,13 +2,17 @@ package com.example.otakuizz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
 import com.example.otakuizz.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     //declara las variables del bindeo
     //se inicia en null, hasta que se infle (inflate)
-    private var _binding : ActivityMainBinding? = null
+    private var _binding: ActivityMainBinding? = null
+
     //puede ser public porque solo es una variable asignada a un getter
     private val binding
         get() = _binding!!
@@ -22,5 +26,19 @@ class MainActivity : AppCompatActivity() {
         binding.tvHello.text = getString(R.string.hello_payer)
         setContentView(view)
 
+
+//incluye un controlador de navegacion. fragmentcontainerView2 es el host de navegación.
+        val navFragment =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        val navController = navFragment.navController
+//establece la barra con controles
+        NavigationUI.setupActionBarWithNavController(this, navController)
+
+    }
+
+    //popupTo en el nav_graph
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = this.findNavController(R.id.fragmentContainerView)
+        return navController.navigateUp()
     }
 }
