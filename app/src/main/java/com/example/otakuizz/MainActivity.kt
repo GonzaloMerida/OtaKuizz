30791ae71fe86766484159da3335c707f9e939c4
Gonzalo <gonzalomerida@hotmail.com>
2023-12-21 12:00:10 +0100

    incluido tema de menu items

--- app/src/main/java/com/example/otakuizz/MainActivity.kt
+++ app/src/main/java/com/example/otakuizz/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.otakuizz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
@@ -17,6 +19,8 @@ class MainActivity : AppCompatActivity() {
     private val binding
         get() = _binding!!
 
+    private lateinit var drawerLayout: DrawerLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //setContentView(R.layout.activity_main)
@@ -26,19 +30,41 @@ class MainActivity : AppCompatActivity() {
         binding.tvHello.text = getString(R.string.hello_payer)
         setContentView(view)
 
+        drawerLayout = binding.drawerLayout
+
 
 //incluye un controlador de navegacion. fragmentcontainerView2 es el host de navegación.
         val navFragment =
-            supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+            supportFragmentManager.findFragmentById(R.id.fragmentContainerView2) as NavHostFragment
         val navController = navFragment.navController
 //establece la barra con controles
-        NavigationUI.setupActionBarWithNavController(this, navController)
+        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
+
+
+//eventos de escucha del menú de navegación...
+        binding.drawerNav.setNavigationItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.rules_menu -> {
+                    navController.navigate(R.id.action_titleFragment_to_rulesFragment)
+                    drawerLayout.closeDrawer(GravityCompat.START)
+                    true
+                }
+
+                R.id.about_menu -> {
+                    navController.navigate(R.id.action_titleFragment_to_aboutFragment)
+                    drawerLayout.closeDrawer(GravityCompat.START)
+                    true
+                }
+
+                else -> false
+            }
+        }
 
     }
 
     //popupTo en el nav_graph
     override fun onSupportNavigateUp(): Boolean {
-        val navController = this.findNavController(R.id.fragmentContainerView)
-        return navController.navigateUp()
+        val navController = this.findNavController(R.id.fragmentContainerView2)
+        return NavigationUI.navigateUp(navController, drawerLayout)
     }
 }
